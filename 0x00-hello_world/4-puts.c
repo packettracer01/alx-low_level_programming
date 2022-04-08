069fd0e5658e94866f1c69dc829d7614377cf7af
Ntichika Humphrey Chisom <ntichika.humphrey.chisom@gmail.com>
2022-04-08 05:16:30 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: 0 after printing the function
- */
+*main - Entry point
+*
+* Return: 0 after printing the function
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+return (0);
 }
